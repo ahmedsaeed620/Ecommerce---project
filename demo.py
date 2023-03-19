f9c66d8f3d18d8518b9e435ddee2b786b18f88d0
ahmedsaeed620 <66470701+ahmedsaeed620@users.noreply.github.com>
2023-03-19 20:53:34 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime as d
+import plotly.express as px
 
 st.set_page_config(layout= 'wide')
 data = pd.read_csv('Ecommerce_clean.csv')
